5b34dd65c5b2f616b65091822bb0a015e082e997
Matthew Sotoudeh <matthewsot@outlook.com>
2014-06-16 19:01:33 -0400

    Basic vNext application
    w/ Identity and EF, using Windows auth to a SQL db.

---
+++ Classfinder/Classfinder/Models/IdentityModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNet.Identity.Entity;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Classfinder.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class UserAccount : User
+    {
+
+    }
+
+    public class CfDb : IdentitySqlContext<UserAccount>
+    {
+        private static bool _created = false;
+        
+        public CfDb(IServiceProvider serviceProvider, IOptionsAccessor<DbContextOptions> optionsAccessor)
+            : base(serviceProvider, optionsAccessor.Options.BuildConfiguration())
+        {
+            // Create the database and schema if it doesn't exist
+            // This is a temporary workaround to create database until Entity Framework database migrations 
+            // are supported in ASP.NET vNext
+            if (!_created)
+            {
+                Database.EnsureCreated();
+                _created = true;
+            }
+        }
+    }
+}
