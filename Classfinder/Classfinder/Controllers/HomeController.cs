5b3119d43694ebd5673b5551302b06d4bdf61f44
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-05 23:30:16 -0400

    basic schedule display

--- Classfinder/Classfinder/Controllers/HomeController.cs
+++ Classfinder/Classfinder/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Classfinder.Models;
@@ -9,6 +10,21 @@ namespace Classfinder.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private CfDb db = new CfDb();
+
+        private ICollection<Class> CheckAndFillWithNoClass(ICollection<Class> schedule)
+        {
+            for (var i = 1; i <= 7; i++)
+            {
+                if (!schedule.Any(@class => @class.Period == i))
+                {
+                    schedule.Add(db.Classes.First(@class => @class.Name == "No Class" && @class.Period == i));
+                }
+            }
+            db.SaveChanges();
+            return schedule;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
@@ -21,15 +37,21 @@ namespace Classfinder.Controllers
         [AllowAnonymous]
         public ActionResult Schedule(string userName)
         {
-            using (var db = new CfDb())
+            var user = db.Users.FirstOrDefault(usr => usr.UserName == userName);
+
+            ViewBag.FirstSemester = CheckAndFillWithNoClass(user.FirstSemester).OrderBy(@class => @class.Period);
+            ViewBag.SecondSemester = CheckAndFillWithNoClass(user.SecondSemester).OrderBy(@class => @class.Period);
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
             {
-                var user = db.Users.FirstOrDefault(usr => usr.UserName == userName);
-
-                ViewBag.FirstSemester = user.FirstSemester;
-                ViewBag.SecondSemester = user.SecondSemester;
-
-                return View();
+                db.Dispose();
             }
+            base.Dispose(disposing);
         }
     }
 }
