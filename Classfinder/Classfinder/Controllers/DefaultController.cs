395c9288373ff9ca9e4f78b9aaf8130f38376b8c
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-05 22:56:27 -0400

    Added a basic login page

--- Classfinder/Classfinder/Controllers/DefaultController.cs
+++ Classfinder/Classfinder/Controllers/DefaultController.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Classfinder.Controllers
 {
     public class DefaultController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("LogIn")]
+        public ActionResult LogIn()
         {
             return View();
         }
