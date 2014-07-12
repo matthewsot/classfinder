08ec5699ac43866ef3dad5f04963fdae31e21704
matthewsotoudeh <matthewsot@outlook.com>
2014-07-11 22:39:20 -0700

    'recorded classes' is more like a grid now

---
+++ Classfinder/Classfinder/Content/Scripts/suffix.js
@@ -0,0 +1,16 @@
+﻿function suffix(num) {
+    var str = num.toString();
+
+    switch(str.substr(str.length - 1)) {
+        case "0":
+            return str;
+        case "1":
+            return str + "st";
+        case "2":
+            return str + "nd";
+        case "3":
+            return str + "rd";
+        default:
+            return str + "th";
+    }
+}
