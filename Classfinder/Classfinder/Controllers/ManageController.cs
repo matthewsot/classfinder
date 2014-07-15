0dd9568f66506214d04d3f8187f541175d04bed3
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-14 23:25:14 -0700

    added new Manage layout
    still working on it, but it should be settings.psd in the mockups folder

---
+++ Classfinder/Classfinder/Controllers/ManageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Classfinder.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        // GET: Manage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+    }
+}
