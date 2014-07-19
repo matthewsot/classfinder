9dbf495ebb4912067f4abb461384eafd19997935
matthewsotoudeh <matthewsot@outlook.com>
2014-07-18 22:12:59 -0700

    modify schedule page finished

--- Classfinder/Classfinder/Controllers/SchedulesAPIController.cs
+++ Classfinder/Classfinder/Controllers/SchedulesAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Http;
 using Classfinder.Models;
+using Microsoft.AspNet.Identity;
 
 namespace Classfinder.Controllers
 {
@@ -124,10 +125,11 @@ namespace Classfinder.Controllers
         }
 
         [HttpPost]
-        [Route("API/Schedule/{userId}/{semester}/{period}")]
-        public IHttpActionResult SetClassForPeriod(ClassModel model, string userId, int semester, int period)
+        [Authorize]
+        [Route("API/Schedule/{semester}/{period}")]
+        public IHttpActionResult SetClassForPeriod(ClassModel model, int semester, int period)
         {
-            var user = db.Users.Find(userId);
+            var user = db.Users.Find(User.Identity.GetUserId());
             var schedule = semester == 2 ? user.SecondSemester : user.FirstSemester;
 
             var currClassInPeriod = schedule.FirstOrDefault(@class => @class.Period == period);
@@ -172,6 +174,52 @@ namespace Classfinder.Controllers
             {
                 good = true,
                 id = schedule.FirstOrDefault(@class => @class.Period == period).Id
+            });
+        }
+
+        //These routes are getting really bad...
+        //TODO: fix the routes
+        [HttpGet]
+        [Authorize]
+        [Route("API/Schedule/CopyFromSemester/{from}/ToSemester/{to}")]
+        public IHttpActionResult CopySemester(int from, int to)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            var fromSchedule = from == 2 ? user.SecondSemester : user.FirstSemester;
+            var toSchedule = to == 2 ? user.SecondSemester : user.FirstSemester;
+
+            for (var period = 1; period <= 7; period++)
+            {
+                //Clear the toSchedule's class for this period
+                //TODO: this should all be in a separate function
+
+                var currClassInPeriod = toSchedule.FirstOrDefault(@class => @class.Period == period);
+                if (currClassInPeriod != null)
+                {
+                    toSchedule.Remove(currClassInPeriod);
+                }
+
+                var classToAdd = fromSchedule.FirstOrDefault(@class => @class.Period == period);
+                if (classToAdd != null)
+                {
+                    toSchedule.Add(classToAdd);
+                }
+
+                if (currClassInPeriod != null &&
+                    currClassInPeriod.Name != "No Class" &&
+                    currClassInPeriod != classToAdd &&
+                    !currClassInPeriod.StudentsInClassFirstSemester.Any() &&
+                    !currClassInPeriod.StudentsInClassSecondSemester.Any())
+                {
+                    db.Classes.Remove(currClassInPeriod);
+                }
+            }
+
+            db.SaveChanges();
+
+            return Ok(new
+            {
+                good = true
             });
         }
 
