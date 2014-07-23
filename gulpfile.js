2ec3f6028d43e9bb2a8fade797f01dfbbe6d171d
matthewsotoudeh <matthewsot@outlook.com>
2014-07-23 16:14:13 -0700

    gulpfile minifies css too

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS = require('gulp-minify-css');
+var rename = require("gulp-rename");
+var gulpignore = require('gulp-ignore');
 
 gulp.task('default', function() {
     gulp.src('./Classfinder/Classfinder/Content/Styles/**/*.less')
         .pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(sourcemaps.write(''))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./Classfinder/Classfinder/Content/Styles'));
+    
+    gulp.src('./Classfinder/Classfinder/Content/Styles/**/*.css')
+        .pipe(gulpignore.exclude(/.*\.min\.css$/, minifyCSS()))
+        .pipe(rename({
+            extname: ".min.css"
+        }))
         .pipe(gulp.dest('./Classfinder/Classfinder/Content/Styles'));
 });
