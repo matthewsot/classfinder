f07d811624a46cf7649045c0ef0033dbffd459c5
matthewsotoudeh <matthewsot@outlook.com>
2014-07-23 12:56:20 -0700

    gulpfile compiles LESS & sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('default', function() {
+    gulp.src('Classfinder/Classfinder/Content/Styles/**/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write('Classfinder/Classfinder/Content/Styles'))
+        .pipe(gulp.dest(''));
 });
