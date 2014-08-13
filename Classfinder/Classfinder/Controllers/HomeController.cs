30b85e11f6722209704f59415dbd087ec3377030
matthewsotoudeh <matthewsot@outlook.com>
2014-08-12 21:15:07 -0700

    added "share" button

--- Classfinder/Classfinder/Controllers/HomeController.cs
+++ Classfinder/Classfinder/Controllers/HomeController.cs
@@ -47,6 +47,7 @@ namespace Classfinder.Controllers
             }
 
             ViewBag.RealName = user.RealName;
+            ViewBag.ViewingUserName = user.UserName;
             ViewBag.UserId = user.Id;
 
             if (User.Identity.IsAuthenticated)
