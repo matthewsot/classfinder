902848a459c461c453a7598798174949402bf5f0
matthewsotoudeh <matthewsot@outlook.com>
2015-04-26 13:36:20 -0700

    moved account API models to their own file

--- Classfinder/Classfinder/Controllers/AccountAPIController.cs
+++ Classfinder/Classfinder/Controllers/AccountAPIController.cs
@@ -10,47 +10,11 @@ using Microsoft.AspNet.Identity.Owin;
 
 namespace Classfinder.Controllers
 {
-    public class SignUpModel
-    {
-        public string Password { get; set; }
-        public int GradYear { get; set; }
-        public string School { get; set; }
-        public string FullName { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-    }
-
-    public class ResetPassModel
-    {
-        public string Username { get; set; }
-    }
-    public class ChangePasswordModel
-    {
-        public string OldPassword { get; set; }
-        public string NewPassword { get; set; }
-    }
-    public class ChangeUsernameModel
-    {
-        public string NewUsername { get; set; }
-    }
-    public class ChangeNameModel
-    {
-        public string NewName { get; set; }
-    }
-
-    public class ResetPasswordModel
-    {
-        public string token { get; set; }
-        public string userId { get; set; }
-        public string newPassword { get; set; }
-    }
-
     [Authorize]
     public class AccountAPIController : ApiController
     {
         private CfDb db = new CfDb();
 
-        //modified from the lhs-campaign source ;) https://github.com/matthewsot/lhs-campaign file /Controllers/AccountsAPIController.cs
         [HttpPost]
         [Route("API/Account/SignUp")]
         [AllowAnonymous]
---
+++ Classfinder/Classfinder/Models/AccountAPIModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Classfinder.Models
+{
+    public class SignUpModel
+    {
+        public string Password { get; set; }
+        public int GradYear { get; set; }
+        public string School { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ResetPassModel
+    {
+        public string Username { get; set; }
+    }
+    public class ChangePasswordModel
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+    public class ChangeUsernameModel
+    {
+        public string NewUsername { get; set; }
+    }
+    public class ChangeNameModel
+    {
+        public string NewName { get; set; }
+    }
+
+    public class ResetPasswordModel
+    {
+        public string token { get; set; }
+        public string userId { get; set; }
+        public string newPassword { get; set; }
+    }
+}
